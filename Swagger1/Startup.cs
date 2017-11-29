744750f11d5d22d0d70cdc44fadb4b1be091a00b
Sahas <Sahas.Lad@appliedis.com>
2017-11-29 13:49:39 +0530

    Added a new .net project & its files
    Added a new .net project & its files

---
+++ Swagger1/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Swagger1.Startup))]
+
+namespace Swagger1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
